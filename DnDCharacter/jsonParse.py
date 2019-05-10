c211fb245b027d0c85edb90c6a4b76c09109c430
riehl046 <riehl046@umn.edu>
2019-05-10 12:44:57 -0500

    json parser continued, doesn't work yet

--- DnDCharacter/jsonParse.py
+++ DnDCharacter/jsonParse.py
@@ -2,9 +2,8 @@ class JsonObject:
     # assumes the text is stored line by
     # line in a list, and only the text between
     # the braces is in the array
-    def __init__(self, jsonText):
+    def __init__(self):
         self.dataTable = {}
-        self.parseText(jsonText)
 
     def __getitem__(self,index):
         return self.dataTable[index]
@@ -14,42 +13,72 @@ class JsonObject:
 
 class JSONParser:
     def __init__(self,string):
+        self.objects = []
+        self.current = None
         self.pos = 0
         self.parse(string)
 
     def parse(self,string):
-        while self.pos < len(string):
+        length = len(string)
+        while self.pos < length:
             if string[self.pos] == '{':
+                self.current = JsonObject()
                 self.pos += 1
                 self.parseObject(string)
-# pretty sure this is actually useless
-##            ignore = 0
-##            notDone = True
-##            while notDone: # this makes this loop skip nested objects
-##                self.pos += 1
-##                if string[self.pos] == '}' and ignore == 0:
-##                    notDone = False
-##                elif string[self.pos] == '{':
-##                    ignore += 1
-##                elif string[self.pos] == '}':
-##                    ignore -= 1
-##            self.pos += 1
+                self.objects.append(self.current)
+                self.current = None
+            else:
+                self.pos += 1
 
     def parseObject(self,string):
-        numerals = '1234567890'
         while string[self.pos] != '}':
             if string[self.pos] == '"':
-                self.parseString(string)
-            elif string[self.pos] == '[':
-                self.parseList(string)
-            elif string[self.pos] in numerals:
-                self.parseNum
+                self.parseKey(string)
+                self.pos += 1
+            else:
+                self.pos += 1
 
-    def parseList(self,string):
-        print("Parsing list")
+    def parseKey(self,string):
+        key = ''
+        while string[self.pos] != '"':
+            key += string[self.pos]
+            self.pos += 1
+        self.current[key] = None
+        self.parseValue(string,key)
+
+    def parseValue(self,string,key): # a value can be a JsonObject {}, a list [], a string "", or a number 99
+        numerals = "1234567890"
+        reserved = '{["'
+        errors = '}]'
+        while string[self.pos] not in numerals or string[self.pos] not in reserved:
+            if string[self.pos] in errors:
+                raise Exception("Invalid JSON formatting")
+            self.pos += 1
+        if string[self.pos] in numerals:
+            self.current[key] = self.parseNumber(string)
+        elif string[self.pos] == '"':
+            self.current[key] = self.parseString(string)
+        elif string[self.pos] == '{':
+            raise Exception("This doesn't work yet, sorry")
+        elif string[self.pos] == '[':
+            self.current[key] = self.parseList(string)
+
+    def parseNumber(self,string):
+        None
 
     def parseString(self,string):
-        print("Parsing string")
+        None
 
-    def parseNum(self,string):
-        print("Parsting Num")
+    def parseList(self,string):
+        None
+
+    def parseNestedObj(self,string):
+        None
+            
+            
+        
+
+            
+        
+        
+        
